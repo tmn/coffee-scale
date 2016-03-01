ca108a307e0fa64d96ef917da0d9ed89ad191178
tmn <mail@trimn.net>
2016-03-01 11:19:27 +0100

    Add Scale class

--- scale/reader.py
+++ scale/reader.py
@@ -3,73 +3,94 @@ import usb.core
 import usb.util
 from sys import exit
 
-def __connect(idVendor, idProduct):
-    # find the USB device
-    dev = usb.core.find(idVendor=idVendor, idProduct=idProduct)
-    if dev is None:
-        return dev
-    else:
-        devmanufacturer = usb.util.get_string(dev, 256, 1)
-        devname = usb.util.get_string(dev, 256, 2)
+class Scale:
+    weight_empty_value = None
+    weight_new_coffee_value = None
+    id_vendor = None
+    id_product = None
+    device = None
 
-        interface = 0
-        if dev.is_kernel_driver_active(interface) is True:
-            dev.detach_kernel_driver(interface)
-            dev.set_configuration()
-            usb.util.claim_interface(dev, interface)
-    return dev
+    def __init__(self, id_vendor, id_product, weight_empty_value=1900, weight_new_coffee_value=3000):
+        self.id_vendor = id_vendor
+        self.id_product = id_product
+        self.weight_empty_value = weight_empty_value
+        self.weight_new_coffee_value = weight_new_coffee_value
 
-def __disconnect(dev):
-    interface = 0
-    usb.util.release_interface(dev, interface)
-          
-def __analyzeData(data):
-    DATA_MODE_GRAMS = 2
-    DATA_MODE_OUNCES = 11
-    grams = 0
-    
-    if data != None:
-        raw_weight = data[4] + data[5] * 256
+        self.device = self.__connect()
 
-    if raw_weight > 0:
-        if data[2] == DATA_MODE_OUNCES:
-            ounces = raw_weight * 0.1
-            grams = round(ounces * 28.3495, 2)
-        elif data[2] == DATA_MODE_GRAMS:
-            grams = raw_weight
-
-        return grams
-
-    return 0
-
-
-def __grabData(dev):
-    # first endpoint
-    endpoint = dev[0][(0,0)][0]
-
-    # read a data packet
-    attempts = 10
-    data = None
-    while data is None and attempts > 0:
-        try:
-            data = dev.read(endpoint.bEndpointAddress,
-                               endpoint.wMaxPacketSize)
-            
-        except usb.core.USBError as e:
-            data = None
-            if e.args == ('Operation timed out',):
-                attempts -= 1
-                # print "timed out... trying again"
-                continue
-
-    return data
-
-
-def establish(idVendor, idProduct):
-    def read():
-        dev = __connect(idVendor, idProduct)
-        if dev is not None:
-            data = __analyzeData(__grabData(dev))
-            __disconnect(dev)
+    def read(self):
+        if self.device is not None:
+            data = self.__analyzeData(self.__grabData())
+            self.__disconnect()
             return data
-    return read
+
+    def is_empty(self):
+        if self.read() <= self.weight_empty_value:
+            return True
+
+        return False
+
+    def has_new_coffee(self):
+        if self.read() >= self.weight_new_coffee_value:
+            return True
+
+        return False
+
+    def __connect(self):
+        # find the USB device
+        self.device = usb.core.find(idVendor=self.id_vendor, idProduct=self.id_product)
+
+        if self.device is None:
+            return self.device
+        else:
+            interface = 0
+
+            if self.device.is_kernel_driver_active(interface) is True:
+                self.device.detach_kernel_driver(interface)
+                self.device.set_configuration()
+                usb.util.claim_interface(self.device, interface)
+
+        return self.device
+
+    def __disconnect(self):
+        interface = 0
+        usb.util.release_interface(self.device, interface)
+
+    def __analyzeData(self, data):
+        DATA_MODE_GRAMS = 2
+        DATA_MODE_OUNCES = 11
+        grams = 0
+
+        if data != None:
+            raw_weight = data[4] + data[5] * 256
+
+        if raw_weight > 0:
+            if data[2] == DATA_MODE_OUNCES:
+                ounces = raw_weight * 0.1
+                grams = round(ounces * 28.3495, 2)
+            elif data[2] == DATA_MODE_GRAMS:
+                grams = raw_weight
+
+            return grams
+
+        return 0
+
+
+    def __grabData(self):
+        # first endpoint
+        endpoint = self.device[0][(0,0)][0]
+
+        # read a data packet
+        attempts = 10
+        data = None
+
+        while data is None and attempts > 0:
+            try:
+                data = self.device.read(endpoint.bEndpointAddress, endpoint.wMaxPacketSize)
+            except usb.core.USBError as e:
+                data = None
+                if e.args == ('Operation timed out',):
+                    attempts -= 1
+                    continue
+
+        return data
